a9a4b1508dd80e0620c255d9c13489ab7da37b22
AnTosi <andrea.tosi.uni@gmail.com>
2021-10-11 13:10:50 +0200

    finito esercizio con fabio js pari e dispari

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 // chiediamo all'utente due parole in successione, veifichiamo quale delle due parole è più lunga e stampiamo in console un messaggio appropriato 
 
-const userAgeOne = parseInt(prompt("Quanti anni hai?"));
+const pari_dispari = prompt("Pari o Dispari?").toLowerCase();
+
+const numero_utente = Number(prompt("digita un numero compreso tra 1 e 9:"));
+
+console.log(pari_dispari, numero_utente);
 
 
-const userAgeTwo = parseInt(prompt("Quanti anni hai però dimmi un altro numero"))
+const pc_number = Math.floor(Math.random()* 8) + 1;
+console.log(pc_number);
 
-if (userAgeOne > userAgeTwo) {
-    console.log ("user1 è più grande di user2");
-}
+const result = numero_utente + pc_number;
 
-if (userAgeTwo > userAgeOne) {
-    console.log ("user2 è più grande di user1")
-}
+if (result % 2 == 0) {
+    console.log(result, "ha vinto pari");
+    var risultato = "pari";
+} else {
+    console.log(result, "ha vinto dispari");
+    var risultato ="dispari";
+    }
 
-if (userAgeOne == userAgeTwo) {
-    console.log ("user1 e user2 hanno la stessa età")
+if (risultato == pari_dispari) {
+    console.log("user wins");
+
+} else {
+    console.log("pc wins");
 }
